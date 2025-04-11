2912bc8e7cfa20a8afd86da24272a43bce663acb
fercortez <cortezfernando238@gmail.com>
2025-04-11 11:07:56 -0300

    uso de directivas

--- src/app/contacto/contacto/contacto.component.ts
+++ src/app/contacto/contacto/contacto.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import {FormGroup,FormControl, FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
 export class ContactoComponent {
+/*
+  usuario={
+    nombre: ''
+  };
+*/
 
+  miFormulario = new FormGroup({
+    email: new FormControl('')
+  });
 }
--- src/app/inicio/inicio/inicio.component.ts
+++ src/app/inicio/inicio/inicio.component.ts
@@ -1,11 +1,27 @@
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [NgFor,NgIf,NgClass,],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
+    /*productos =[
+      { nombre: 'producto 1', precio: 100},
+      { nombre: 'producto 2', precio: 150},
+      { nombre: 'producto 3', precio: 200}
+    ];*/
+   
+    usuario = {
+      nombre: 'ari',
+      /*isLoggedIn:true*/
+      activo: true
+    };
+
+
+
+
 
 }
